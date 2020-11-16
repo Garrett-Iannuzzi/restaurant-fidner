279659806aad944768db855aec881b16f6619be8
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-11-16 16:04:09 -0700

    Add fetch method for data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { apiKey } from '../../apiKey'
+ 
 
-function App() {
+const requestHeader: any = {
+  "Content-Type": "application/json",
+  "Authorization": apiKey
+}
+
+const App: React.FC = () => {
+  const [ restaurants, getRestaurants ] = useState<any>([]);
+
+  useEffect(() => {
+    fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
+      headers: requestHeader
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(data => getRestaurants(data))
+  })
+
   return (
     <div className="App">
       <h1>Restaurant Finder</h1>
